038ab80d76bdc2515c93d273392563c5f63edfb5
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2023-01-15 16:50:42 -0300

    Adicionar arquivos de projeto.

---
+++ Automacao-Parte1/Program.cs
@@ -0,0 +1,20 @@
+﻿using EasyAutomationFramework;
+using OpenQA.Selenium;
+using System;
+
+namespace Automacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var web = new Web();
+            web.StartBrowser();
+            web.Navigate("https://google.com");
+            web.AssignValue(TypeElement.Name, "q", "pinterest").element.SendKeys(Keys.Enter);
+
+            //web.CloseBrowser();
+
+        }
+    }
+}
